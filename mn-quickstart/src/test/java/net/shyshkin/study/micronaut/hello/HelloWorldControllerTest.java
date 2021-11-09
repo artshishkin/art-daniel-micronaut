1797cbd904aa25ec483735493aaf657367beffeb
Art <d.art.shishkin@gmail.com>
2021-11-09 19:53:38 +0200

    7. Your first Micronaut Test Experience (#1)

---
+++ mn-quickstart/src/test/java/net/shyshkin/study/micronaut/hello/HelloWorldControllerTest.java
@@ -0,0 +1,47 @@
+package net.shyshkin.study.micronaut.hello;
+
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest
+class HelloWorldControllerTest {
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+    @Test
+    void helloWorldEndpointRespondsWithProperContent() {
+
+        //given
+        String expectedResponse = "Hello World Art";
+
+        //when
+        String response = client.toBlocking().retrieve("/hello");
+
+        //then
+        assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    void helloWorldEndpointRespondsWithProperStatusAnd() {
+
+        //given
+        var expectedStatus = HttpStatus.OK;
+
+        //when
+        var response = client.toBlocking().exchange("/hello", String.class);
+
+        //then
+        assertEquals(expectedStatus, response.status());
+        MediaType mediaType = response.getContentType().orElseThrow();
+        assertEquals(MediaType.TEXT_PLAIN, mediaType.getName());
+    }
+}
