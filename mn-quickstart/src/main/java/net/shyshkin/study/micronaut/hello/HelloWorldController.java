d31d670e4c9a1981a6d001dc9a88b7c87ec795d9
Art <d.art.shishkin@gmail.com>
2021-11-09 21:33:19 +0200

    13. Logging (#1)

--- mn-quickstart/src/main/java/net/shyshkin/study/micronaut/hello/HelloWorldController.java
+++ mn-quickstart/src/main/java/net/shyshkin/study/micronaut/hello/HelloWorldController.java
@@ -3,9 +3,13 @@ package net.shyshkin.study.micronaut.hello;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/hello")
 public class HelloWorldController {
+
+    public static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
 
     private final MyService service;
 
@@ -15,6 +19,7 @@ public class HelloWorldController {
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String helloWorld() {
+        LOG.debug("Called the hello World API");
         return this.service.helloFromService();
     }
 
