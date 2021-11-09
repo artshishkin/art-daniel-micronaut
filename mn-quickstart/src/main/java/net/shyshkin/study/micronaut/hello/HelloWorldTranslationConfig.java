16a95766e0b741c4759990aa9bc9b25803800f53
Art <d.art.shishkin@gmail.com>
2021-11-09 23:22:51 +0200

    16.2 Immutable Configuration Injection - Validation (#1)

--- mn-quickstart/src/main/java/net/shyshkin/study/micronaut/hello/HelloWorldTranslationConfig.java
+++ mn-quickstart/src/main/java/net/shyshkin/study/micronaut/hello/HelloWorldTranslationConfig.java
@@ -2,12 +2,16 @@ package net.shyshkin.study.micronaut.hello;
 
 import io.micronaut.context.annotation.ConfigurationProperties;
 
+import javax.validation.constraints.NotBlank;
+
 @ConfigurationProperties("hello.world.translation")
 public interface HelloWorldTranslationConfig {
 
     //de
+    @NotBlank
     String getDe();
 
     //en
+    @NotBlank
     String getEn();
 }
