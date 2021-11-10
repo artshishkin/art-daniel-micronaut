90371565219463b1642e2d4ede61d06c8973c38f
Art <d.art.shishkin@gmail.com>
2021-11-10 18:00:00 +0200

    34.2 Reactive HTTP Request Processing - compare execution thread with non-reactive (#2)

--- mn-stock-broker/src/main/java/net/shyshkin/study/micronaut/broker/account/WatchListController.java
+++ mn-stock-broker/src/main/java/net/shyshkin/study/micronaut/broker/account/WatchListController.java
@@ -4,6 +4,7 @@ package net.shyshkin.study.micronaut.broker.account;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.micronaut.broker.model.WatchList;
 import net.shyshkin.study.micronaut.store.AccountStore;
 
@@ -11,6 +12,7 @@ import java.util.UUID;
 
 import static io.micronaut.http.MediaType.APPLICATION_JSON;
 
+@Slf4j
 @Controller("/account/watchlist")
 @RequiredArgsConstructor
 public class WatchListController {
@@ -21,6 +23,7 @@ public class WatchListController {
 
     @Get(produces = APPLICATION_JSON)
     public WatchList get() {
+        log.debug("GET /account/watchlist");
         return store.getWatchList(ACCOUNT_ID);
     }
 
@@ -29,11 +32,13 @@ public class WatchListController {
             produces = APPLICATION_JSON
     )
     public WatchList update(@Body WatchList watchList) {
+        log.debug("PUT /account/watchlist");
         return store.updateWatchList(ACCOUNT_ID, watchList);
     }
 
     @Delete("/{accountId}")
     public HttpResponse<?> delete(@PathVariable UUID accountId) {
+        log.debug("DELETE /account/watchlist");
         store.deleteWatchList(accountId);
         return HttpResponse.noContent();
     }
