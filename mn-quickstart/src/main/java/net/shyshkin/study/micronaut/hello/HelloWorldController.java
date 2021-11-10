5ef351e564d854d38fcc68052b1a10f5d656badb
Art <d.art.shishkin@gmail.com>
2021-11-10 09:48:39 +0200

    27. Integrate Lombok (#1)

--- mn-quickstart/src/main/java/net/shyshkin/study/micronaut/hello/HelloWorldController.java
+++ mn-quickstart/src/main/java/net/shyshkin/study/micronaut/hello/HelloWorldController.java
@@ -4,13 +4,11 @@ import io.micronaut.context.annotation.Property;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller("/hello")
 public class HelloWorldController {
-
-    public static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
 
     private final String helloFromConfig;
 
@@ -29,19 +27,19 @@ public class HelloWorldController {
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String helloWorld() {
-        LOG.debug("Called the hello World API");
+        log.debug("Called the hello World API");
         return this.service.helloFromService();
     }
 
     @Get(uri = "/config", produces = MediaType.TEXT_PLAIN)
     public String config() {
-        LOG.debug("Called the config API");
+        log.debug("Called the config API");
         return helloFromConfig;
     }
 
     @Get(uri = "/translation", produces = MediaType.APPLICATION_JSON)
     public HelloWorldTranslationConfig translation() {
-        LOG.debug("Called the translation API");
+        log.debug("Called the translation API");
         return translationConfig;
     }
 
