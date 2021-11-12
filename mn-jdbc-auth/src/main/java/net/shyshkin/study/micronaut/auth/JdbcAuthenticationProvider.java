f4fff7b9bcb92ffa031d8a50e2d2137a47b7aee0
Art <d.art.shishkin@gmail.com>
2021-11-12 12:58:13 +0200

    40.7 JDBC Authentication Provider - Auth Provider check logic (#3)

--- mn-jdbc-auth/src/main/java/net/shyshkin/study/micronaut/auth/JdbcAuthenticationProvider.java
+++ mn-jdbc-auth/src/main/java/net/shyshkin/study/micronaut/auth/JdbcAuthenticationProvider.java
@@ -5,12 +5,20 @@ import io.micronaut.security.authentication.*;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import jakarta.inject.Singleton;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.micronaut.auth.persistence.UserEntity;
+import net.shyshkin.study.micronaut.auth.persistence.UserRepository;
 import org.reactivestreams.Publisher;
 
+import java.util.Optional;
+
 @Slf4j
+@RequiredArgsConstructor
 @Singleton
 public class JdbcAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserRepository repository;
 
     @Override
     public Publisher<AuthenticationResponse> authenticate(HttpRequest<?> httpRequest, AuthenticationRequest<?, ?> authenticationRequest) {
@@ -18,6 +26,22 @@ public class JdbcAuthenticationProvider implements AuthenticationProvider {
         Object secret = authenticationRequest.getSecret();
         log.debug("User {} tries to login ...", identity);
         return Flowable.create(emitter -> {
+
+            Optional<UserEntity> maybeUser = repository.findByEmail(identity);
+            if (maybeUser.isPresent()) {
+                UserEntity user = maybeUser.get();
+                log.debug("Found user: {}", user.getEmail());
+                if (user.getPassword().equals(secret)) {
+                    log.debug("User logged in.");
+                    emitter.onNext(AuthenticationResponse.success(user.getEmail()));
+                    emitter.onComplete();
+                    return;
+                } else {
+                    log.debug("Wrong password provided for user: {}", identity);
+                }
+            } else {
+                log.debug("No user found with email: {}", identity);
+            }
             emitter.onError(new AuthenticationException(new AuthenticationFailed("Wrong username or password")));
         }, BackpressureStrategy.ERROR);
     }
