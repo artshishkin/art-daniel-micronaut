82f7019079a2bee82e20e0aa1e22a927ac6ea09d
Art <d.art.shishkin@gmail.com>
2021-11-12 22:34:16 +0200

    45.3 Micronaut Data JPA - Controller Endpoint (#4)

--- mn-stock-broker/src/main/java/net/shyshkin/study/micronaut/broker/MarketsController.java
+++ mn-stock-broker/src/main/java/net/shyshkin/study/micronaut/broker/MarketsController.java
@@ -5,11 +5,14 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
+import io.reactivex.Single;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.micronaut.broker.persistence.jpa.SymbolEntity;
+import net.shyshkin.study.micronaut.broker.persistence.jpa.SymbolsRepository;
 import net.shyshkin.study.micronaut.store.InMemoryStore;
 
 import java.util.List;
@@ -20,6 +23,7 @@ import java.util.List;
 public class MarketsController {
 
     private final InMemoryStore inMemoryStore;
+    private final SymbolsRepository repository;
 
     @Operation(summary = "Return all available markets")
     @ApiResponse(
@@ -29,5 +33,15 @@ public class MarketsController {
     @Get
     public List<Symbol> all() {
         return inMemoryStore.getAllSymbols();
+    }
+
+    @Operation(summary = "Return all available markets from database using JPA")
+    @ApiResponse(
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    @Tag(name = "markets") //to group together
+    @Get("/jpa")
+    public Single<List<SymbolEntity>> allSymbolsViaJpa() {
+        return Single.just(repository.findAll());
     }
 }
