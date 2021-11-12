46e31af37a7cce6e11f9985f1f2dc4640c04f20b
Art <d.art.shishkin@gmail.com>
2021-11-12 22:48:21 +0200

    45.4 Micronaut Data JPA - TestDataProvider (#4)

---
+++ mn-stock-broker/src/main/java/net/shyshkin/study/micronaut/broker/persistence/jpa/TestDataProvider.java
@@ -0,0 +1,31 @@
+package net.shyshkin.study.micronaut.broker.persistence.jpa;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.env.Environment;
+import io.micronaut.context.event.StartupEvent;
+import io.micronaut.runtime.event.annotation.EventListener;
+import lombok.RequiredArgsConstructor;
+
+import javax.inject.Singleton;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Singleton
+@Requires(notEnv = Environment.TEST)
+@RequiredArgsConstructor
+public class TestDataProvider {
+
+    private final SymbolsRepository symbolsRepository;
+
+    @EventListener
+    void init(StartupEvent event) {
+
+        if (symbolsRepository.count() == 0) {
+            var symbols = Stream.of("APPL", "AMZN", "FB", "GOOG", "MSFT", "NFLX", "TSLA")
+                    .map(SymbolEntity::new)
+                    .collect(Collectors.toList());
+            symbolsRepository.saveAll(symbols);
+        }
+    }
+
+}
