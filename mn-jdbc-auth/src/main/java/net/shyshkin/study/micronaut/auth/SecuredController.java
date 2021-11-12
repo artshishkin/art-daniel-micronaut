ba04d46ea354c45016142a53cf812b5b7493d23d
Art <d.art.shishkin@gmail.com>
2021-11-12 11:30:08 +0200

    40.3 JDBC Authentication Provider - Secured Endpoint (#3)

---
+++ mn-jdbc-auth/src/main/java/net/shyshkin/study/micronaut/auth/SecuredController.java
@@ -0,0 +1,17 @@
+package net.shyshkin.study.micronaut.auth;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+
+@Secured(SecurityRule.IS_AUTHENTICATED)
+@Controller("/secured")
+public class SecuredController {
+
+    @Get("/status")
+    public String status() {
+        return "You are authenticated!";
+    }
+
+}
