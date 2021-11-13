c10275617e66400997cfd9a38cc66aa039b22173
Art <d.art.shishkin@gmail.com>
2021-11-13 15:02:03 +0200

    46.2 Populate Quotes Test Data (#4)

--- mn-stock-broker/src/main/java/net/shyshkin/study/micronaut/broker/persistence/jpa/TestDataProvider.java
+++ mn-stock-broker/src/main/java/net/shyshkin/study/micronaut/broker/persistence/jpa/TestDataProvider.java
@@ -5,9 +5,12 @@ import io.micronaut.context.env.Environment;
 import io.micronaut.context.event.StartupEvent;
 import io.micronaut.runtime.event.annotation.EventListener;
 import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.micronaut.broker.persistence.model.QuoteEntity;
 import net.shyshkin.study.micronaut.broker.persistence.model.SymbolEntity;
 
 import javax.inject.Singleton;
+import java.math.BigDecimal;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,6 +20,7 @@ import java.util.stream.Stream;
 public class TestDataProvider {
 
     private final SymbolsRepository symbolsRepository;
+    private final QuotesRepository quotesRepository;
 
     @EventListener
     void init(StartupEvent event) {
@@ -26,7 +30,26 @@ public class TestDataProvider {
                     .map(SymbolEntity::new)
                     .collect(Collectors.toList());
             symbolsRepository.saveAll(symbols);
+
+            symbols.stream()
+                    .map(this::initRandomQuote)
+                    .forEach(quotesRepository::save);
         }
     }
+
+    private QuoteEntity initRandomQuote(SymbolEntity symbol) {
+        return QuoteEntity.builder()
+                .symbol(symbol)
+                .bid(randomValue())
+                .ask(randomValue())
+                .lastPrice(randomValue())
+                .volume(randomValue())
+                .build();
+    }
+
+    private BigDecimal randomValue() {
+        return BigDecimal.valueOf(ThreadLocalRandom.current().nextDouble(1, 100));
+    }
+
 
 }
