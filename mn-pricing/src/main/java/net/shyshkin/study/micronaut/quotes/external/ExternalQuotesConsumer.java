1f0ffbd7d58f346f8272d7e4c3cb9fa0424391e0
Art <d.art.shishkin@gmail.com>
2021-11-14 23:06:33 +0200

    60.2 Kafka Consumer - in batch (#5)

--- mn-pricing/src/main/java/net/shyshkin/study/micronaut/quotes/external/ExternalQuotesConsumer.java
+++ mn-pricing/src/main/java/net/shyshkin/study/micronaut/quotes/external/ExternalQuotesConsumer.java
@@ -1,25 +1,22 @@
 package net.shyshkin.study.micronaut.quotes.external;
 
-import io.micronaut.configuration.kafka.annotation.KafkaKey;
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
 import io.micronaut.configuration.kafka.annotation.Topic;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 @Slf4j
 @KafkaListener(
         clientId = "mn-pricing-external-quote-consumer",
-        groupId = "external-quote-consumer"
+        groupId = "external-quote-consumer",
+        batch = true
 )
 public class ExternalQuotesConsumer {
 
     @Topic("external-quotes")
-    public void receive(@KafkaKey String symbol,
-                        ExternalQuote externalQuote,
-                        long offset,
-                        int partition,
-                        String topic,
-                        long timestamp) {
-        log.debug("Got {} quote: {}. Offset: {}", symbol, externalQuote, offset);
+    public void receive(List<ExternalQuote> externalQuotes) {
+        log.debug("Consuming batch of external quotes {}", externalQuotes);
     }
 
 }
