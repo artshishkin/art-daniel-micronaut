f9e43792f9ba1e6d5cf29c38ed3431ae28831173
Art <d.art.shishkin@gmail.com>
2021-11-18 17:01:07 +0200

    12.4.1 Modified CronJobHandler to use ScheduledEvent and updated JodaTime (#12)

--- mn-tutorial-cron-jobs-lambda/src/test/java/net/shyshkin/study/micronaut/CronJobHandlerTest.java
+++ mn-tutorial-cron-jobs-lambda/src/test/java/net/shyshkin/study/micronaut/CronJobHandlerTest.java
@@ -1,9 +1,12 @@
 package net.shyshkin.study.micronaut;
 
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+import org.joda.time.DateTime;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -25,17 +28,29 @@ public class CronJobHandlerTest {
 
     @Test
     public void testHandler() {
-        Map<String, Object> event = Map.of(
-                "version", 0,
-                "id", UUID.randomUUID(),
-                "detail-type", "Scheduled Event",
-                "source", "aws.events",
-                "account", 1234567899,
-                "time", "2021-05-19T09:03:02Z",
-                "region", "us-east-1",
-                "resources", "[arn:aws:events:us-east-1:1234567899:rule/5minutes]",
-                "detail", "{}"
-        );
+
+//        Map<String, Object> event = Map.of(
+//                "version", 0,
+//                "id", UUID.randomUUID(),
+//                "detail-type", "Scheduled Event",
+//                "source", "aws.events",
+//                "account", 1234567899,
+//                "time", "2021-05-19T09:03:02Z",
+//                "region", "us-east-1",
+//                "resources", "[arn:aws:events:us-east-1:1234567899:rule/5minutes]",
+//                "detail", "{}"
+//        );
+
+        ScheduledEvent event = new ScheduledEvent()
+                .withId(UUID.randomUUID().toString())
+                .withDetailType("Scheduled Event")
+                .withSource("aws.events")
+                .withAccount("1234567899")
+                .withTime(DateTime.now())
+                .withRegion("us-east-1")
+                .withResources(List.of("arn:aws:events:us-east-1:1234567899:rule/5minutes"))
+                .withDetail(Map.of());
+
         cronJobHandler.execute(event);
     }
 }
