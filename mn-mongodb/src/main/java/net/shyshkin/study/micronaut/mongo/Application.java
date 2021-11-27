961a3ad077fc7cd213e18e5a7ca1b9897aa53f7f
Art <d.art.shishkin@gmail.com>
2021-11-27 18:37:55 +0200

    69.1 Micronaut MongoDB - init (#9)

---
+++ mn-mongodb/src/main/java/net/shyshkin/study/micronaut/mongo/Application.java
@@ -0,0 +1,10 @@
+package net.shyshkin.study.micronaut.mongo;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
