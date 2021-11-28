b61b9ac07c31f3de5daf6fe62100f73557a6d96f
Art <d.art.shishkin@gmail.com>
2021-11-28 10:43:53 +0200

    70.1 Micronaut Elastic Search - Setup - Init (#9)

---
+++ mn-elastic-search/src/main/java/net/shyshkin/study/micronaut/elasticsearch/Application.java
@@ -0,0 +1,10 @@
+package net.shyshkin.study.micronaut.elasticsearch;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
