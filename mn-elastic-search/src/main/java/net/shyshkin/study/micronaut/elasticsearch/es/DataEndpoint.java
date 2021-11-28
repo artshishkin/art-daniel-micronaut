84029ea35e5247f824deab7bf076b134dce35df6
Art <d.art.shishkin@gmail.com>
2021-11-28 15:58:14 +0200

    71.2 REST API - asynchronous non-blocking call (#9)

--- mn-elastic-search/src/main/java/net/shyshkin/study/micronaut/elasticsearch/es/DataEndpoint.java
+++ mn-elastic-search/src/main/java/net/shyshkin/study/micronaut/elasticsearch/es/DataEndpoint.java
@@ -6,6 +6,7 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -16,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Controller("/data")
 public class DataEndpoint {
@@ -38,6 +40,26 @@ public class DataEndpoint {
         Map<String, Object> source = response.getSource();
         log.debug("Requested Document by id {}: {}", documentId, response.getSourceAsString());
         return response.getSourceAsString();
+    }
+
+    @Get("/document/async/{id}")
+    public CompletableFuture<String> byIdAsync(@PathVariable("id") UUID documentId) {
+        CompletableFuture<String> whenDone = new CompletableFuture<>();
+        client.getAsync(new GetRequest(this.index, documentId.toString()), RequestOptions.DEFAULT, new ActionListener<GetResponse>() {
+            @Override
+            public void onResponse(GetResponse response) {
+                var source = response.getSourceAsString();
+                log.debug("Requested Document by id {}: {}", documentId, source);
+                whenDone.complete(source);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                log.error("Error while requesting document by id {}: {}", documentId, e.getMessage());
+                whenDone.completeExceptionally(e);
+            }
+        });
+        return whenDone;
     }
 
 }
